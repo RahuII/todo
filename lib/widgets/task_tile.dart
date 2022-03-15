d91e8ae44aa50763615393fca8bc59dafcf13f2b
rahul138 <rahul.kumar17407@marwadieducation.edu.in>
2022-03-15 10:41:41 +0530

    working checkbox

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -12,29 +12,37 @@ class _TaskTileState extends State<TaskTile> {
 
   @override
   Widget build(BuildContext context) {
-    return const ListTile(
-      title: Text('This is the task.'),
-      trailing: TaskCheckbox(
-        checkboxState: false,
+    return ListTile(
+      title: Text(
+        'This is the task.',
+        style: TextStyle(
+            decoration: isChecked ? TextDecoration.lineThrough : null),
       ),
+      trailing: TaskCheckbox(
+          checkboxState: isChecked,
+          togglecheckboxState: (bool? checkboxState) {
+            setState(() {
+              isChecked = checkboxState!;
+            });
+          }),
     );
   }
 }
 
 class TaskCheckbox extends StatelessWidget {
-  const TaskCheckbox({Key? key, required this.checkboxState}) : super(key: key);
+  const TaskCheckbox(
+      {Key? key,
+      required this.checkboxState,
+      required this.togglecheckboxState})
+      : super(key: key);
   final bool checkboxState;
+  final Function(bool?) togglecheckboxState;
   @override
   Widget build(BuildContext context) {
     return Checkbox(
       activeColor: Colors.lightBlueAccent,
-      // fillColor: MaterialStateProperty.resolveWith(getColor),
       value: checkboxState,
-      onChanged: (newValue) {
-        // setState(() {
-        //   isChecked = newValue!;
-        // });
-      },
+      onChanged: togglecheckboxState,
     );
   }
 }
